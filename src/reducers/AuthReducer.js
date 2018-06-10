78a1c1a8e695d891f19ddc0db2182398ffebb8f1
j-ornelas <johnnyoh1988@gmail.com>
2018-06-09 19:47:56 -0700

    Adds logic for error messaging and loading state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,18 +1,45 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER,
+} from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
   password: '',
+  loading: false,
+  error: '',
+  user: null,
 };
 
 export default (state = INITIAL_STATE, action) => {
+  console.log('action', action);
   switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' };
     case EMAIL_CHANGED:
       // we have to use ...state to make a new object
       // in memory to compare old state vs new state.
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        user: action.payload,
+        error: '',
+      };
+    case LOGIN_USER_FAIL:
+      console.log('login user fail payload', action.payload.code);
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.code,
+        password: '',
+      };
     default:
       return state;
   }
