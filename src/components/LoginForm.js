413ebcab1a6a1b810f3bad7049e924411d854b0c
j-ornelas <johnnyoh1988@gmail.com>
2018-06-09 19:46:50 -0700

    Adds loading and error msg to login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
-import { Card, CardSection, Button, Input } from './common';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
+import { Card, CardSection, Button, Input, Spinner } from './common';
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+};
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -10,7 +19,35 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
-    console.log(this.props)
+    console.log(this.props);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+    // loginUser expects an object, hence this weird syntax
+    this.props.loginUser({ email, password });
+  }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View>
+          <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+        </View>
+      );
+    }
+    return null;
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner />;
+    }
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
   }
 
   render() {
@@ -31,14 +68,14 @@ class LoginForm extends Component {
             label="password"
             placeholder="p4s5w0rD"
             onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
           />
         </CardSection>
 
         <CardSection>
-          <Button>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
+        {this.renderError()}
       </Card>
     );
   }
@@ -47,6 +84,11 @@ class LoginForm extends Component {
 const mapStateToProps = state => ({
   email: state.auth.email,
   password: state.auth.password,
+  loading: state.auth.loading,
+  error: state.auth.error,
+  user: state.auth.user,
 });
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser,
+})(LoginForm);
